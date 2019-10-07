416b42a215d0d5300aa192e5250228efd43205f3
ankitaluthra19 <ankitaluthra19@gmail.com>
2019-10-07 16:23:57 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,127 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js");
+
+if (workbox) {
+    console.log("Yay! Workbox is loaded !");
+    workbox.precaching.precacheAndRoute([]);
+
+    /*  cache images in the e.g others folder; edit to other folders you got
+    and config in the sw-config.js file
+    */
+    workbox.routing.registerRoute(
+        /(.*)others(.*)\.(?:png|gif|jpg)/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "images",
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+    /* Make your JS and CSS âš¡ fast by returning the assets from the cache,
+     while making sure they are updated in the background for the next use.
+    */
+    workbox.routing.registerRoute(
+    // cache js, css, scc files
+        /.*\.(?:css|js|scss|)/,
+        // use cache but update in the background ASAP
+        new workbox.strategies.StaleWhileRevalidate({
+            // use a custom cache name
+            cacheName: "assets",
+        })
+    );
+
+    // cache google fonts
+    workbox.routing.registerRoute(
+        new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+        new workbox.strategies.CacheFirst({
+            cacheName: "google-fonts",
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+
+    // add offline analytics
+    workbox.googleAnalytics.initialize();
+
+    /* Install a new service worker and have it update
+   and control a web page as soon as possible
+    */
+
+    workbox.core.skipWaiting();
+    workbox.core.clientsClaim();
+
+} else {
+    console.log("Oops! Workbox didn't load ðŸ‘º");
+}
+
+
+//This is the "Offline copy of pages" wervice worker
+//Install stage sets up the index page (home page) in the cahche and opens a new cache
+self.addEventListener('install', function(event) {
+  var indexPage = new Request('index.html');
+  event.waitUntil(
+    fetch(indexPage).then(function(response) {
+      return caches.open('pwabuilder-offline').then(function(cache) {
+        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+        return cache.put(indexPage, response);
+      });
+  }));
+});
+//If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener('fetch', function(event) {
+  var updateCache = function(request){
+    return caches.open('pwabuilder-offline').then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log('[PWA Builder] add page to offline'+response.url)
+        return cache.put(request, response);
+      });
+    });
+  };
+  event.waitUntil(updateCache(event.request));
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+      //Check to see if you have it in the cache
+      //Return response
+      //If not in the cache, then return error page
+      return caches.open('pwabuilder-offline').then(function (cache) {
+        return cache.match(event.request).then(function (matching) {
+          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+          return report
+        });
+      });
+    })
+  );
+})
+
+
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+});
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
